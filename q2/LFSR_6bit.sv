9954311a9652c356afdca8278fb75af039caf852
davidamantunes <davidamantunes@gmail.com>
2025-09-19 20:21:59 +0200

    ex2

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -1,10 +1,33 @@
 module LFSR_6bit (
-  input  logic clk, rst_n,
-  input  logic sel,
-  input  logic [5:0] parallel_in,
-  output logic [5:0] parallel_out
+    input  logic clk, rst_n,
+    input  logic sel,
+    input  logic [5:0] parallel_in,
+    output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+
+    // …
+    // Add your description here
+    // …
+    logic [5:0] parallel_out_next;
+
+    always_comb begin : comb_logic
+        if (sel) begin
+            parallel_out_next[0] = parallel_out[5];
+            parallel_out_next[1] = parallel_out[0]^parallel_out[5];
+            parallel_out_next[2] = parallel_out[1];
+            parallel_out_next[3] = parallel_out[2]^parallel_out[5];
+            parallel_out_next[4] = parallel_out[3];
+            parallel_out_next[5] = parallel_out[4];
+        end else begin
+            parallel_out_next = parallel_in;
+        end
+    end
+
+    always_ff @( posedge clk or negedge rst_n ) begin : N_size_register
+        if (!rst_n)
+            parallel_out <= 6'b0;
+        else
+            parallel_out <= parallel_out_next;
+    end
+
 endmodule
--- q2/LFSR_6bit_tb.sv
+++ q2/LFSR_6bit_tb.sv
@@ -1,5 +1,34 @@
 module LFSR_6bit_tb;
 
-// complete here
+    logic clk, rst_n;
+    logic sel;
+    logic [5:0] parallel_in;
+    logic [5:0] parallel_out;
 
+    LFSR_6bit uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .sel(sel),
+        .parallel_in(parallel_in),
+        .parallel_out(parallel_out)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 1'b0; rst_n = 1'b0; sel = 1'b0; parallel_in = 6'b000000;
+        #12 rst_n = 1'b1; // ativar
+        #30 sel = 1'b0; parallel_in = 6'b011010;
+        #30 sel = 1'b1; // LFSR mode do 3 clk cycles
+        #30 sel = 1'b0; parallel_in = 6'b111111;
+        #10 sel = 1'b1;
+        #30 sel = 1'b0; parallel_in = 6'b000000;
+        #10 sel = 1'b1;
+        #30 $stop;
+    end
+
+    initial begin
+        $monitor("Time=%0t | clk=%b rst_n=%b sel=%b parallel_in=%h parallel_out=%h",
+                 $time, clk, rst_n, sel, parallel_in, parallel_out);
+    end
 endmodule
