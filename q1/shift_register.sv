4838fa7ead4a327fc32a3eb537d8547917549e45
davidamantunes <davidamantunes@gmail.com>
2025-09-19 15:57:14 +0200

    ex1

---
+++ q1/shift_register.sv
@@ -0,0 +1,34 @@
+module shift_register #(
+    parameter int N=4
+) (
+    input logic clk,
+    input logic rst_n,
+    input logic serial_parallel,
+    input logic load_enable,
+    input logic serial_in,
+    input logic [N-1:0] parallel_in,
+    output logic [N-1:0] parallel_out,
+    output logic serial_out
+);
+
+logic [N-1:0] parallel_out_next;
+
+//If load_enable then parallel_out_next = parallel_in, else parallel_out_next = parallel_out
+// If 0, load serially; if 1, load parallel
+assign parallel_out_next = (load_enable&serial_parallel) ? parallel_in :
+                        (load_enable&(~serial_parallel)) ? {serial_in, parallel_out[N-1:1]} :
+                        parallel_out;
+
+assign serial_out = parallel_out[0];
+
+//complete here
+
+always_ff @( posedge clk or negedge rst_n ) begin : N_size_register
+    if (!rst_n)
+        parallel_out <= {N{1'b0}};
+    else
+        parallel_out <= parallel_out_next;
+end
+
+
+endmodule
--- q1/shift_register_tb.sv
+++ q1/shift_register_tb.sv
@@ -1,5 +1,53 @@
 module shift_register_tb;
 
+    localparam int N = 4;
+
+    logic clk, rst_n, serial_parallel, load_enable, serial_in;
+    logic [N-1:0] parallel_in;
+    logic [N-1:0] parallel_out;
+    logic serial_out;
+
+    shift_register #(
+        .N(N)
+    ) uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .serial_parallel(serial_parallel),
+        .load_enable(load_enable),
+        .serial_in(serial_in),
+        .parallel_in(parallel_in),
+        .parallel_out(parallel_out),
+        .serial_out(serial_out)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 1'b0; rst_n = 1'b0; load_enable = 1'b0; parallel_in = 4'h0;
+        serial_parallel = 1'b0; serial_in = 1'b0;
+        #12 rst_n = 1'b1; // ativar
+        #10 load_enable = 1'b1; serial_parallel = 1'b1; parallel_in = 4'h5;
+        #10 load_enable = 1'b0; serial_parallel = 1'b1; parallel_in = 4'hC;
+        #10 load_enable = 1'b1; serial_parallel = 1'b1; parallel_in = 4'hE;
+        #10 load_enable = 1'b1; serial_parallel = 1'b1; parallel_in = 4'hF;
+        #10 rst_n = 1'b0; load_enable = 1'b1; serial_parallel = 1'b1; parallel_in = 4'h5;
+        #12 rst_n = 1'b1; // ativar
+        #10 load_enable = 1'b1; parallel_in = 4'hF;
+        #10 load_enable = 1'b1; serial_parallel = 1'b1; parallel_in = 4'hF;
+        #10 load_enable = 1'b1; serial_parallel = 1'b0; serial_in = 1'b0;
+        #10 load_enable = 1'b1; serial_parallel = 1'b0; serial_in = 1'b1;
+        #10 load_enable = 1'b0; serial_parallel = 1'b0; serial_in = 1'b1;
+        #10 load_enable = 1'b1; serial_parallel = 1'b0; serial_in = 1'b0;
+        #10 load_enable = 1'b1; serial_parallel = 1'b0; serial_in = 1'b1;
+        #10 rst_n = 1'b0; load_enable = 1'b1; serial_parallel = 1'b0; serial_in = 1'b1;
+        #30 $stop;
+    end
+
+    initial begin
+        $monitor("Time=%0t | clk=%b rst_n=%b load_enable=%b parallel_in=%h parallel_out=%h", 
+                 $time, clk, rst_n, load_enable, parallel_in, parallel_out);
+    end
+
 // complete here
 
 endmodule
